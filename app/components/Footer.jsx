45686b604f97be481c54651a6a5afe2ba65153e4
Tanakorn <tanakorn@Tanakorn.local>
2025-10-20 21:34:59 +0700

    update6

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
   return (
    <>
-   <div className='bg-green-400 p-3'>
-    <h5 className='flex flex-row justify-center '>footer</h5>
+   <div className='bg-green-600 p-3'>
+    <h5 className='flex flex-row justify-center text-center text-white text-sm'> © {new Date().getFullYear()} LINE GIRL. All rights reserved.</h5>
    </div>
    </>
   )
